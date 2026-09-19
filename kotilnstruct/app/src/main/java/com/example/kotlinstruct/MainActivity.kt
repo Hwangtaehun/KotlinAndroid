4ab513d1af85baf7c430c9e988a3131481529f9c
Hwangtaehun <r283@naver.com>
2026-09-19 20:10:33 +0900

    레이아웃 XML로 화면 구성

--- kotilnstruct/app/src/main/java/com/example/kotlinstruct/MainActivity.kt
+++ kotilnstruct/app/src/main/java/com/example/kotlinstruct/MainActivity.kt
@@ -13,30 +13,7 @@ import androidx.core.content.ContextCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // 이름 문자열 출력 TextView 생성
-        val name = TextView(this).apply {
-            typeface = Typeface.DEFAULT_BOLD
-            text = "Lake Louise"
-        }
-        // 이미지 출력 ImageView 생성
-        val image = ImageView(this).also {
-            it.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.lake_1))
-        }
-        // 주소 문자열 출력 TextView 생성
-        val address = TextView(this).apply {
-            typeface = Typeface.DEFAULT_BOLD
-            text = "Lake Louise, AB, 캐나다"
-        }
-
-        val layout = LinearLayout(this).apply {
-            orientation = LinearLayout.VERTICAL
-            gravity = Gravity.CENTER
-            // LinearLayout 객체에 TextView 객체 추가
-            addView(name, WRAP_CONTENT, WRAP_CONTENT)
-            addView(image, WRAP_CONTENT, 600)
-            addView(address, WRAP_CONTENT, WRAP_CONTENT)
-        }
-
-        setContentView(layout)
+        // 화면 출력 XML 명시
+        setContentView(R.layout.activity_main)
     }
 }

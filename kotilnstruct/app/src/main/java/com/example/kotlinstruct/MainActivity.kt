1c33b8bba37bcc7537e8716a1b818a26f96bc5e4
Hwangtaehun <r283@naver.com>
2026-09-19 19:44:42 +0900

    액티비티 코드로 화면 구성

--- kotilnstruct/app/src/main/java/com/example/kotlinstruct/MainActivity.kt
+++ kotilnstruct/app/src/main/java/com/example/kotlinstruct/MainActivity.kt
@@ -1,21 +1,42 @@
 package com.example.kotlinstruct
 
 import android.graphics.Typeface
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        // 이름 문자열 출력 TextView 생성
+        val name = TextView(this).apply {
+            typeface = Typeface.DEFAULT_BOLD
+            text = "Lake Louise"
         }
+        // 이미지 출력 ImageView 생성
+        val image = ImageView(this).also {
+            it.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.lake_1))
+        }
+        // 주소 문자열 출력 TextView 생성
+        val address = TextView(this).apply {
+            typeface = Typeface.DEFAULT_BOLD
+            text = "Lake Louise, AB, 캐나다"
+        }
+
+        val layout = LinearLayout(this).apply {
+            orientation = LinearLayout.VERTICAL
+            gravity = Gravity.CENTER
+            // LinearLayout 객체에 TextView 객체 추가
+            addView(name, WRAP_CONTENT, WRAP_CONTENT)
+            addView(image, WRAP_CONTENT, 600)
+            addView(address, WRAP_CONTENT, WRAP_CONTENT)
+        }
+
+        setContentView(layout)
     }
 }

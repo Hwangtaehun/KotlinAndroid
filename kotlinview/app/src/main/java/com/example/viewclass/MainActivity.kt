397b93d9fb60fafa8046104a3348b2dc652b8660
Hwangtaehun <r283@naver.com>
2026-09-19 20:55:01 +0900

    뷰의 크기 지정

--- kotlinview/app/src/main/java/com/example/viewclass/MainActivity.kt
+++ kotlinview/app/src/main/java/com/example/viewclass/MainActivity.kt
@@ -5,11 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val textView1: TextView = findViewById(R.id.text1)
+        val textView2: TextView = findViewById<TextView>(R.id.text1)
     }
 }

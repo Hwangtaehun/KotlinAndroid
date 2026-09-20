2ab3834b7e435c7cdcea95241913deaa6b6cd8d7
Hwangtaehun <r283@naver.com>
2026-09-20 19:42:33 +0900

    뷰 바인딩

--- kotlinview/app/src/main/java/com/example/viewclass/MainActivity.kt
+++ kotlinview/app/src/main/java/com/example/viewclass/MainActivity.kt
@@ -6,13 +6,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.TextView
+import com.example.viewclass.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val textView1: TextView = findViewById(R.id.text1)
-        val textView2: TextView = findViewById<TextView>(R.id.text1)
+        // 바인딩 객체 획득
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        // 액티비티 화면 출력
+        setContentView(binding.root)
+
+        // 뷰 객체 이용
+        binding.visibleBn.setOnClickListener {
+            binding.targetView.visibility = TextView.VISIBLE
+        }
+        binding.invisibleBn.setOnClickListener {
+            binding.targetView.visibility = TextView.INVISIBLE
+        }
     }
 }
